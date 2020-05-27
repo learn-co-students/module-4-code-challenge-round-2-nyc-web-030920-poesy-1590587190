153dc23dd66bbfb516cf3100fd0a086cf0804075
nick-echevarria <nick.echev@gmail.com>
2020-05-27 10:18:03 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,64 @@ import "./App.css";
 import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
+const allPoemsUrl = "http://localhost:6001/poems";
+const headers = {
+  "content-type": "application/json", 
+  "accept": "application/json"
+}
+
 class App extends React.Component {
+  state = { 
+    allPoems: [], 
+    visible: false, 
+    submittedPoem: []
+  }
+
+  componentDidMount() { 
+    this.fetchPoems()
+  }
+
+  fetchPoems = () => { 
+    fetch(allPoemsUrl)
+      .then(res => res.json())
+      .then(allPoems => { 
+        this.setState({ allPoems })
+      })
+  }
+
+  toggleForm = () => { 
+    this.setState({visible: !this.state.visible})
+  }
+  
+  submitNewPoem = (event, newPoem) => { 
+    event.preventDefault;
+
+    fetch(allPoemsUrl, { 
+      method: "POST", 
+      headers, 
+      body: JSON.stringify({ 
+        title: newPoem.title, 
+        content: newPoem.content, 
+        author: newPoem.author
+      })
+    })
+      .then(res => res.json())
+      .then(submittedPoem => { 
+        let allPoemsCopy = [...this.state.allPoems]
+        this.setState({ 
+          allPoems: 
+         })
+      })
+  }
+
   render() {
     return (
       <div className="app">
         <div className="sidebar">
-          <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          <button onClick={this.toggleForm} >Show/hide new poem form</button>
+          {this.state.visible === true && <NewPoemForm submitNewPoem={this.submitNewPoem} /> }
         </div>
-        <PoemsContainer />
+        <PoemsContainer allPoems={this.state.allPoems} />
       </div>
     );
   }
--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -1,13 +1,25 @@
 import React from "react";
 
 class NewPoemForm extends React.Component {
+  state = { 
+    title: "", 
+    author: "", 
+    content: ""
+  }
+
+  handleForm = (event) => { 
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   render() {
     return (
       <form className="new-poem-form">
-        <input placeholder="Title" />
-        <input placeholder="Author" />
-        <textarea placeholder="Write your masterpiece here..." rows={10} />
-        <input type="submit" value="Share your masterpiece" />
+        <input name="title" onChange={(event) => this.handleForm(event)} value={this.state.title} placeholder="Title" />
+        <input name="author" onChange={(event) => this.handleForm(event)} value={this.state.author} placeholder="Author" />
+        <textarea name="content" onChange={(event) => this.handleForm(event)} value={this.state.content} placeholder="Write your masterpiece here..." rows={10} />
+        <input onClick={(event) => this.props.submitPoem(event, this.state)} type="submit" value="Share your masterpiece" />
       </form>
     );
   }
