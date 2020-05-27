f3a1a96726fc9208e3b9b4e18f94361aae263bc0
Ramzy Badawy <Ramzy.Badawy@gmail.com>
2020-05-27 10:31:39 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,36 @@ import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
 class App extends React.Component {
+
+  state={
+    poems: [],
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:6001/poems')
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({
+        poems: data
+      })
+    })
+  }
+
+  addForm = (newpoems) => {
+    this.setState({
+      poems:[...this.state.poems,newpoems]
+    })
+  }
+
   render() {
+    // console.log("First Console Log", this.state.poems)
     return (
       <div className="app">
         <div className="sidebar">
           <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          {false && <NewPoemForm addForm={this.addForm} />}
         </div>
-        <PoemsContainer />
+        <PoemsContainer poems={this.state.poems} />
       </div>
     );
   }
--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -1,12 +1,48 @@
 import React from "react";
 
 class NewPoemForm extends React.Component {
+  state = {
+    title: '',
+    author: '',
+    content: '',
+  }
+
+    handleChange = (event) => {
+      this.setState({
+        [event.target.name]:event.target.value
+      })
+    }
+
+    handleSubmit = (event) => {
+      event.preventDefault()
+
+      fetch('http://localhost:6001/poems', {
+        method: "POST",
+        headers:{
+          "content-type" : "application/json",
+          accepts: "application/json"
+        },
+        body: JSON.stringify({
+          title:this.state.title,
+          author: this.state.author,
+          content: this.state.content
+        })
+      })
+      .then(resp => resp.json())
+      .then(newPoem => this.props.addForm(newPoem))
+      .then(this.setState({
+        title: '',
+        author: '',
+        content: '',
+      }))
+    }
   render() {
+    // console.log('round 2',this.state)
     return (
-      <form className="new-poem-form">
-        <input placeholder="Title" />
-        <input placeholder="Author" />
-        <textarea placeholder="Write your masterpiece here..." rows={10} />
+      <form className="new-poem-form" onSubmit={this.handleSubmit}>
+        <input placeholder="Title" name="title" value={this.state.title} onChange={this.handleChange} />
+        <input placeholder="Author" name="author" value={this.state.author} onChange={this.handleChange}/>
+        <textarea placeholder="Write your masterpiece here..." rows={10} name="content" value={this.state.content} onChange={this.handleChange}/>
         <input type="submit" value="Share your masterpiece" />
       </form>
     );
