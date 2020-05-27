582514a5f1cbf7aa0350986fa55e955b31ed898b
Leon Jamison <leonjamison@icloud.com>
2020-05-27 10:29:14 -0400

    1st and 2nd ddel

--- src/App.js
+++ src/App.js
@@ -3,19 +3,39 @@ import "./App.css";
 import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
+const get_Backend = `http://localhost:6001/poems`
+
 class App extends React.Component {
 
+  state={
+    poems: [],
+    show:false
+  }
 
+  componentDidMount(){
+    this.getpoems()
+  }
 
+  getpoems=()=>{
+      fetch(get_Backend)
+      .then(r=>r.json())
+      .then(data => this.setState({poems: data}))
+
+  }
+
+  toggle=()=>{
+    this.setState((currentState)=> ({show: !currentState.show}))
+  }
 
   render() {
+    console.log('app state:', this.state)
     return (
       <div className="app">
         <div className="sidebar">
-          <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          <button onClick={this.toggle}> Show/hide new poem form</button>
+          {this.state.show && <NewPoemForm />}
         </div>
-        <PoemsContainer />
+        <PoemsContainer poems={this.state.poems} />
       </div>
     );
   }
--- src/PoemsContainer.js
+++ src/PoemsContainer.js
@@ -3,10 +3,11 @@ import Poem from "./Poem";
 
 class PoemsContainer extends React.Component {
   render() {
+    // console.log("poemcontainer Prop:", this.props)
     return (
       <div className="poems-container">
         {
-          // render poems here
+         this.props.poems.map(poem => <Poem {...poem} key={poem.id}/>)
         }
       </div>
     );
